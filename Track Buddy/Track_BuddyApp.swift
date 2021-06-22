8787b889169dfdf8dacefe82b1372d422d9fc3e8
Daniel Luo <d.luo117@gmail.com>
2021-06-22 15:03:08 -0700

    Create Xcode projecta

---
+++ Track Buddy/Track_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Track_BuddyApp.swift
+//  Track Buddy
+//
+//  Created by Daniel Luo on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Track_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
