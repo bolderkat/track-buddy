bdba4110642d3f6b22669b2913cfa6eba6a0045f
Daniel Luo <d.luo117@gmail.com>
2021-06-28 20:00:52 -0700

    Move drawing constants to Metrics enum

--- Track Buddy/New Group/AccelerometerGraph.swift
+++ Track Buddy/New Group/AccelerometerGraph.swift
@@ -9,20 +9,29 @@ import Foundation
 import SwiftUI
 
 struct AccelerometerGraph: View {
+    private enum Metrics {
+        // MARK: Drawing Constants
+        static let outerToInnerCircleDiamaterRatio = 20.0
+        static let outerEdgeGValue = 3.0 // circle size doesn't appear to correspond with this set G value when graph is resized, check your maffs :(
+        static let tracerLineWidth = 1.0
+    }
+    
     @ObservedObject private(set) var motionManager: MotionManager
     
     var body: some View {
         GeometryReader { geometry in
             let bounds = min(geometry.size.width, geometry.size.height)
-            let pointScaleFactor = -bounds / outerEdgeGValue
+            
+            // Multiply accelerometer G values so they are displayed at a meaningful scale on screen
+            let pointScaleFactor = -bounds / Metrics.outerEdgeGValue
             let xPosition = motionManager.x * pointScaleFactor
             let yPosition = motionManager.z * pointScaleFactor
-            let innerCircleDiameter = bounds / outerToInnerCircleDiamaterRatio
+            let innerCircleDiameter = bounds / Metrics.outerToInnerCircleDiamaterRatio
             
             ZStack(alignment: .center) {
                 Circle()
                 Path(motionManager.pointPath(atScale: pointScaleFactor))
-                    .stroke(Color.red, lineWidth: tracerLineWidth)
+                    .stroke(Color.red, lineWidth: Metrics.tracerLineWidth)
                     .offset(x: geometry.size.width / 2, y: geometry.size.height / 2)
                 Circle()
                 // Vertical axis corresponds with car acceleration/deceleration (z axis in Core Motion)
@@ -35,12 +44,6 @@ struct AccelerometerGraph: View {
     }
     
     
-
-    
-    // MARK: Drawing Constants
-    private let outerToInnerCircleDiamaterRatio = 20.0
-    private let outerEdgeGValue = 3.0 // circle size doesn't appear to correspond with this set G value when graph is resized, check your maffs :(
-    private let tracerLineWidth = 1.0
 }
 
 struct AccelerometerGraph_Previews: PreviewProvider {
